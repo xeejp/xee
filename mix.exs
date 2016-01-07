6c1d3bff5b81013f6215f6ce22478a9073331ecc
ryo33 <ryo12redstone@gmail.com>
2016-01-07 21:33:34 +0900

    Add onetime token servers for hosts and participants

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Xee.Mixfile do
      {:phoenix_html, "~> 1.4"},
      {:phoenix_live_reload, "~> 0.5", only: :dev},
      {:cowboy, "~> 1.0"},
-     {:uuid, "~> 1.1" }]
+     {:uuid, "~> 1.1" },
+     {:onetime, "~> 0.1.2"}]
   end
 end
