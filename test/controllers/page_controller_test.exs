01e2d704a9ad92e4d80937cfd3e09e206cf2d91a
kage-mac <kagemiku@gmail.com>
2016-01-07 22:40:47 +0900

    Recover tests.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Xee.PageControllerTest do
 
   test "GET /" do
     conn = get conn(), "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert conn.status == 200
   end
 end
