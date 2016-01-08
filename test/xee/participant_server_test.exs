b6db73540c5aacafa70cdfa19f9c6a0f6680a38f
ryo33 <ryo12redstone@gmail.com>
2016-01-09 00:41:20 +0900

    Modify tests of ParticipantServer

--- test/xee/participant_server_test.exs
+++ test/xee/participant_server_test.exs
@@ -1,6 +1,6 @@
 defmodule Xee.ParticipantServerTest do
   use ExUnit.Case, async: true
-  alias Xee.ParticipantServer, as: ParticipantServer
+  alias Xee.ParticipantServer
 
   setup do
     ParticipantServer.start_link()
@@ -16,42 +16,29 @@ defmodule Xee.ParticipantServerTest do
     assert :C  == ParticipantServer.get(:c)
   end
 
-  test "register and get, reregistration" do
-    assert :ok == ParticipantServer.register(:a, :A)
-    assert :ok == ParticipantServer.register(:b, :B)
-    assert :ok == ParticipantServer.register(:c, :C)
+  test "reregistration" do
+    ParticipantServer.register(:a, :A)
+    ParticipantServer.register(:b, :B)
+
     assert :A  == ParticipantServer.get(:a)
-    assert :B  == ParticipantServer.get(:b)
-    assert :C  == ParticipantServer.get(:c)
     assert :ok == ParticipantServer.register(:a, :aA)
-    assert :ok == ParticipantServer.register(:b, :bB)
-    assert :ok == ParticipantServer.register(:c, :cC)
     assert :aA == ParticipantServer.get(:a)
-    assert :bB == ParticipantServer.get(:b)
-    assert :cC == ParticipantServer.get(:c)
-  end
 
-  test "register and drop" do
-    assert :ok == ParticipantServer.register(:a, :A)
-    assert :ok == ParticipantServer.register(:b, :B)
-    assert :ok == ParticipantServer.register(:c, :C)
-    assert :ok == ParticipantServer.drop(:a)
-    assert :ok == ParticipantServer.drop(:b)
-    assert :ok == ParticipantServer.drop(:c)
-  end
-
-  test "register and get, drop" do
-    assert :ok == ParticipantServer.register(:a, :A)
-    assert :ok == ParticipantServer.register(:b, :B)
-    assert :ok == ParticipantServer.register(:c, :C)
-    assert :A  == ParticipantServer.get(:a)
     assert :B  == ParticipantServer.get(:b)
-    assert :C  == ParticipantServer.get(:c)
+    assert :ok == ParticipantServer.register(:b, :bB)
+    assert :bB == ParticipantServer.get(:b)
+  end
+
+  test "drop" do
+    ParticipantServer.register(:a, :A)
+    ParticipantServer.register(:b, :B)
+
+    assert :A  == ParticipantServer.get(:a)
     assert :ok == ParticipantServer.drop(:a)
-    assert :ok == ParticipantServer.drop(:b)
-    assert :ok == ParticipantServer.drop(:c)
     assert nil == ParticipantServer.get(:a)
+
+    assert :B  == ParticipantServer.get(:b)
+    assert :ok == ParticipantServer.drop(:b)
     assert nil == ParticipantServer.get(:b)
-    assert nil == ParticipantServer.get(:c)
   end
 end
