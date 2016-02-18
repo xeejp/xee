69d2b834d60e799abd2f9a49d1fa3cd173f62936
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-02-18 22:04:31 +0900

    Fix bugs

--- lib/xee/theme_server.ex
+++ lib/xee/theme_server.ex
@@ -3,13 +3,22 @@ defmodule Xee.ThemeServer do
   The server to store experiment themes.
   """
 
-  def experiment(name, file: file, host: host, participant: participant) do
+  def experiment(name, file: file, host: host, participant: participant, granted: granted) do
     [{module, _} | _] = Code.load_file(file)
     host = File.read!(host)
     participant = File.read!(participant)
     experiment = %Xee.Experiment{theme_id: name, module: module, host: host, participant: participant}
-    theme = %Xee.Theme{experiment: experiment, id: name, name: name, playnum: 0, lastupdate: 0, producer: "", contact: "", manual: ""}
+    granted = if granted == nil do
+      nil
+    else
+      MapSet.new(granted)
+    end
+    theme = %Xee.Theme{experiment: experiment, id: name, name: name, playnum: 0, lastupdate: 0, producer: "", contact: "", manual: "", granted: granted}
     Xee.ThemeServer.register(name, theme)
+  end
+
+  def experiment(name, file: file, host: host, participant: participant) do
+    experiment(name, file: file, host: host, participant: participant, granted: nil)
   end
 
   def start_link() do
