27f695d407a9a19203d7dc928e2815bc7f7af952
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-02-22 18:07:40 +0900

    Fix dependencies

--- mix.exs
+++ mix.exs
@@ -39,6 +39,6 @@ defmodule Xee.Mixfile do
      {:poison, "~> 1.5"},
      {:comeonin, "~> 1.6"},
      {:onetime, "~> 0.1.2"},
-     {:fwatch, "~> 0.3.0", only: :dev}]
+     {:fwatch, "~> 0.4.0"}]
   end
 end
