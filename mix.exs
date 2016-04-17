e9f65f7c29bc046e91272c5f0bba60d3a3ea60b8
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-04-17 12:19:01 +0900

    Replace brunch with webpack
    Fix #68

--- mix.exs
+++ mix.exs
@@ -29,11 +29,12 @@ defmodule Xee.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.15"},
-     {:phoenix_ecto, "~> 0.8"},
+    [{:phoenix, "~> 1.1.4"},
+     {:phoenix_ecto, "~> 2.0"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 1.4"},
-     {:phoenix_live_reload, "~> 0.5", only: :dev},
+     {:phoenix_html, "~> 2.4"},
+     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:gettext, "~> 0.9"},
      {:cowboy, "~> 1.0"},
      {:uuid, "~> 1.1" },
      {:poison, "~> 1.5"},
