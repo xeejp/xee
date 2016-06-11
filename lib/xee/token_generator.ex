dfe628d42fb256850191f6e560e8489a59cc4a31
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-06-11 13:03:07 +0900

    Shorten IDs of participants and experiments
    Fix #59

--- lib/xee/token_generator.ex
+++ lib/xee/token_generator.ex
@@ -2,7 +2,21 @@ defmodule Xee.TokenGenerator do
   @doc """
   Generate 'Universally Unique Identifier' with version 4.
   """
+
   def generate() do
-    UUID.uuid4()
+    {unique, ""} = UUID.uuid4(:hex) |> Integer.parse(16)
+    shorten_hex(unique + 1)
+  end
+
+  def shorten_hex(hex) do
+    characters = Enum.to_list(?0..?9) ++ Enum.to_list(?a..?z)
+    to_string shorten(hex, List.to_tuple(characters))
+  end
+
+  def shorten(0, _), do: []
+
+  def shorten(x, characters) do
+    size = tuple_size(characters)
+    [elem(characters, rem(x, size)) | shorten(div(x, size), characters)]
   end
 end
--- test/xee/token_generator_test.exs
+++ test/xee/token_generator_test.exs
@@ -1,6 +1,7 @@
 defmodule TokenGeneratorTest do
   use ExUnit.Case, async: true
   test "generate" do
-    assert String.length(Xee.TokenGenerator.generate) == 36
+    assert String.length(Xee.TokenGenerator.generate) >= 1
+    assert Xee.TokenGenerator.generate != Xee.TokenGenerator.generate
   end
 end
