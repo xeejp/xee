7fefb3711d58fec4dc89b1297b171a2ab3d004bd
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-08-31 17:12:21 +0900

    Add the page to show descriptions of each themes
    Close #90

--- web/controllers/theme_controller.ex
+++ web/controllers/theme_controller.ex
@@ -8,4 +8,19 @@ defmodule Xee.ThemeController do
               |> Enum.filter(fn theme -> Xee.Theme.granted? theme end)
     render conn, "list.html", themes: themes
   end
+
+  def theme(conn, %{"theme_id" => theme_id}) do
+    %{description: description, id: id, name: name} = Xee.ThemeServer.get(theme_id)
+    unless is_nil(description) do
+      javascript = "/theme/#{theme_id}/description.js"
+      render conn, "theme.html", id: id, name: name, description: description, javascript: javascript
+    else
+      render conn, "no_description.html", id: id, name: name
+    end
+  end
+
+  def description_script(conn, %{"theme_id" => theme_id}) do
+    %{description: js} = Xee.ThemeServer.get(theme_id)
+    render conn, "description.js", javascript: js
+  end
 end
