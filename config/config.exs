35d3458376d2e05667dd4ac844c96b52addc4196
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-09-07 11:19:17 +0900

    Add translation for Japanese

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,23 @@
 # is restricted to this project.
 use Mix.Config
 
+config :xee, Xee.Gettext, default_locale: "ja"
+
+config :xee, ecto_repos: [Xee.Repo]
+
+# Configures the endpoint
+config :xee, Xee.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "YMHjvC+X66HvbtH0da1w6v5rJh+wvPjUW7Iqaq0c7aQgxk1uOp5o0DLz+wW4BD5d",
+  render_errors: [default_format: "html"],
+  pubsub: [name: Xee.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
 defmodule Xee.ThemeConfig do
   defmacro __using__(_) do
     quote do
@@ -44,26 +61,7 @@ defmodule Xee.ThemeConfig do
   end
 end
 
-config :xee, ecto_repos: [Xee.Repo]
-
-# Configures the endpoint
-config :xee, Xee.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "YMHjvC+X66HvbtH0da1w6v5rJh+wvPjUW7Iqaq0c7aQgxk1uOp5o0DLz+wW4BD5d",
-  render_errors: [default_format: "html"],
-  pubsub: [name: Xee.PubSub,
-           adapter: Phoenix.PubSub.PG2]
-
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-# Configures experiments definition files
-config :xee, Xee.ThemeServer,
-  files: ["config/experiments.exs"]
-
-
+# Configures theme definition files
 config :xee, Xee.ThemeServer,
   module_themes: [],
   file_themes: []
