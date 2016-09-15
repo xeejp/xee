c3742f0306d10fd52a5164f19a4c3d5143d48245
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-09-15 16:22:14 +0900

    :put_litter_in_its_place:

--- config/themes.exs
+++ config/themes.exs
@@ -1,17 +1,10 @@
 use Mix.Config
 use Xee.ThemeConfig
 
-# theme Test3,
-#   name: "Test4",
-#   path: "experiments/test3",
+# theme PublicGoods,
+#   name: "公共財",
+#   path: "apps/xeepublicgoods",
 #   host: "host.js",
 #   participant: "participant.js"
-
- theme PublicGoods,
-   name: "PublicGoods",
-   path: "apps/public-goods",
-   host: "host.js",
-   participant: "participant.js"
-
 
 register_themes
