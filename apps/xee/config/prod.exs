d775b4a4a2fa8baecb927e831603f124a416492d
Ryohei HAYASHI <hayashi@xee.jp>
2016-10-21 23:03:49 +0900

    Fixed footer bug.

--- apps/xee/config/prod.exs
+++ apps/xee/config/prod.exs
@@ -15,6 +15,7 @@ config :xee, Xee.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  server: true
 
 # ## SSL Support
 #
