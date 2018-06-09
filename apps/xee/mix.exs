2f140d576bc94e90d330aba212ab67682458e0a5
Ryohei HAYASHI <hayashi@xee.jp>
2018-06-09 23:27:44 +0900

    update latest deps

--- apps/xee/mix.exs
+++ apps/xee/mix.exs
@@ -8,11 +8,12 @@ defmodule Xee.Mixfile do
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     aliases: aliases(),
      deps: deps()]
   end
 
@@ -39,20 +40,20 @@ defmodule Xee.Mixfile do
   #
   # Type `mix help deps` for examples and options
   def deps do
-    [{:phoenix, "~> 1.3.0"},
+    [{:phoenix, "~> 1.3.2"},
      {:xeethemescript, "~> 0.3.0"},
      {:phoenix_pubsub, "~> 1.0.2"},
-     {:phoenix_ecto, "~> 3.2.3"},
-     {:postgrex, ">= 0.13.3"},
-     {:phoenix_html, "~> 2.10.4"},
-     {:phoenix_live_reload, "~> 1.1.1", only: :dev},
-     {:gettext, "~> 0.13.1"},
-     {:cowboy, "~> 1.1.2"},
-     {:uuid, "~> 1.1.7" },
-     {:poison, "~> 2.0"},
-     {:comeonin, "~> 2.6.0"},
+     {:phoenix_ecto, "~> 3.3.0"},
+     {:postgrex, ">= 0.13.5"},
+     {:phoenix_html, "~> 2.11.2"},
+     {:phoenix_live_reload, "~> 1.1.5", only: :dev},
+     {:gettext, "~> 0.15.0"},
+     {:cowboy, "~> 1.0"},
+     {:uuid, "~> 1.1.8" },
+     {:poison, "~> 3.1.0"},
+     {:comeonin, "~> 4.1.1"},
      {:fwatch, "~> 0.5.0"},
-     {:timex, "~> 3.1.24"},
-     {:tzdata, "~> 0.5.12"}]
+     {:timex, "~> 3.3.0"},
+     {:tzdata, "~> 0.5.16"}]
   end
 end
