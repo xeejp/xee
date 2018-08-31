669e5a8030948e6d37ee0d33d3b37b4abdc34001
unknown <hayashi@xee.jp>
2018-08-31 19:24:48 +0900

    bug fix

--- config/themes.exs
+++ config/themes.exs
@@ -9,36 +9,20 @@ use Xee.ThemeConfig
 #   {"その他の実験", []}
 # ]
 
-# theme PublicGoods,
-#   name: "公共財",
-#   path: "apps/xeepublicgoods",
-#   host: "host.js",
-#   participant: "participant.js",
-#   standing_experiments: %{name: "PublicGoods sample", x_token: "sample"},
-#   tags: ["相互作用あり"]
+theme PublicGoods,
+  name: "公共財",
+  path: "apps/xee_public_goods",
+  host: "host.js",
+  participant: "participant.js",
+  standing_experiments: %{name: "PublicGoods sample", x_token: "a"},
+  tags: ["相互作用あり"]
 
 theme DoubleAuction,
   name: "ダブルオークション",
   path: "apps/xee_double_auction",
   host: "host.js",
   participant: "participant.js",
-  standing_experiments: %{name: "Double Auction sample", x_token: "a"},
+  standing_experiments: %{name: "Double Auction sample", x_token: "b"},
   tags: ["相互作用あり"]
-
-theme PublicGoods,
-  name: "公共財",
-  path: "apps/xee_public_goods",
-  host: "host.js",
-  participant: "participant.js",
-  standing_experiments: %{name: "PublicGoods sample", x_token: "b"},
-  tags: ["相互作用あり"]
-
-theme BeautyContest,
-  name: "美人投票ゲーム",
-  path: "apps/xee_beauty_contest",
-  host: "host.js",
-  participant: "participant.js",
-  standing_experiments: %{name: "Beauty Contest sample", x_token: "c"},
-  tags: ["相互作用なし"]
 
 register_themes()
